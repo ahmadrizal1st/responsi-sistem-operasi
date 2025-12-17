6c2c5508a2fc35d1bcb6c0b00ec226255a8d6404
Edo Adetya <averadetya4512@gmail.com>
2025-12-17 10:19:25 +0000

    Update branch Edo/241054023

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # MENJELASKAN MASING-MASING MATERI DARI MODUL
 
-- MANAGEMENT USER
-- 
+- BAB 1: <br>
+whoami => digunakan untuk cek user yang sedang login<br>
+adduser => digunakan untuk menambahkan user baru<br><br>
+- BAB 2: <br>
+mkdir => digunakan untuk membuat folder directory baru <br>
+nano => digunakan untuk menambahkan file baru + mengedit file tersebut <br><br>
+- BAB 3: <br>
+nano /etc/issue => digunakan untuk menjadi template yg diprint ketika login pertama lagi <br>
+nano /etc/motd => digunakan untuk menjadi template yg diprint ketika sudah selesai login <br><br>
+- BAB 4: <br>
+gzip -n => digunakan untuk mengkompres file <br>
+gunzip => dekompres file <br> <br>
+- BAB 5: <br>
+cat => digunakan untuk membuka isi konten file tersebut<br>
+ps => digunakan untuk melihat proses yg berjalan <br><br>
+- BAB 6: <br>
+ps -u => digunakan untuk melihat proses yg berjalan di user saat ini <br>
+ps -u user => digunakan untuk melihat proses yg berjalan untuk pilihan user secara spesifik <br><br>X
--- responsi.sh
+++ responsi.sh
@@ -1,1 +1,135 @@
 ISI FILE INI 
+
+#!/bin/bash
+
+echo "RESPONSI SISTEM OPERASI"
+echo "1. BAB 1: Perintah Dasar Sistem Operasi Linux"
+echo "2. BAB 2: Sistem File"
+echo "3. BAB 3: Konfigurasi Linux"
+echo "4. BAB 4: Utilitas Linux"
+echo "5. BAB 5: Proses Input Output"
+echo "6. BAB 6: Manajemen Proses"
+read -p "Pilih: " pilihan
+
+case $pilihan in
+	1)
+		echo "1. whoami"
+		echo "2. adduser"
+		read -p "Pilih: " pilih1
+
+			case $pilih1 in
+				1)
+					echo "User yang sedang login:"
+					whoami
+					;;
+				2)
+					read -p "Masukkan nama user yg ingin dibuat: " user
+					adduser "$user"
+					;;
+				*)
+					echo "Pilihan tidak valid!"
+					;;
+		esac
+		;;
+	2)
+		echo "1. mkdir"
+		echo "2. nano"
+		read -p "Pilih: " pilih2
+
+			case $pilih2 in
+				1)
+					read -p "Masukkan nama folder yg ingin dibuat: " dir
+					mkdir "$dir"
+					echo "Folder $dir berhasil dibuat"
+					;;
+				2)
+					read -p "Masukkan nama file yg ingin dibuat: " file
+					nano "$file"
+					;;
+				*)
+					echo "Pilihan tidak valid!"
+					;;
+		esac
+		;;
+	3)
+		echo "1. Konfigurasi Run Level awal"
+		echo "2. Konfigurasi Run Level akhir"
+		read -p "Pilih: " pilih3
+
+			case $pilih3 in
+				1)
+					echo "Backup Selesai"
+					sudo cp /etc/issue /etc/issue.backup
+					echo "Edit: "
+					sudo nano /etc/issue
+					;;
+				2)
+					echo "Backup Selesai"
+					sudo cp /etc/motd /etc/motd.backup
+					echo "Edit: "
+					sudo nano /etc/motd
+					;;
+				*)
+					echo "Pilihan tidak valid!"
+					;;
+		exac
+		;;
+	4)
+		echo "1. gzip"
+		echo "2. gunzip"
+		read -p "Pilih: " pilih4
+
+			case $pilih4 in
+				1)
+					read -p "Nama file yg ingin dikompres (pastikan sudah membuat): " fileZip
+					gzip -n "$fileZip" > hasilKompres.gz
+					;;
+				2)
+					read -p "Nama file yg ingin diextract (pastikan sudah membuat): " fileExtract
+					gunzip "$fileExtract"
+					;;
+				*)
+					echo "Pilihan tidak valid!"
+					;;
+		exac
+		;;
+	5)
+		echo "1. cat"
+		echo "2. ps"
+		read -p "Pilih: " pilih5
+
+			case $pilih5 in
+				1)
+					read -p "Nama file yg ingin dibuka (pastikan sudah membuat): " fileCat
+					cat "$fileCat"
+					;;
+				2)
+					echo "Proses yg berjalan: "
+					ps
+					;;
+				*)
+					echo "Pilihan tidak valid!"
+					;;
+		exac
+		;;
+	6)
+		echo "1. ps -u"
+		echo "2. ps -u user"
+		read -p "Pilih: " pilih6
+
+			case $pilih6 in
+				1)
+					echo "Kondisi proses: "
+					ps -u
+					;;
+				2)
+					read -p "Masukkan Nama User: " psUser
+					echo "Kondisi proses $psUser"
+					ps -u "$psUser"
+					;;
+				*)
+					echo "Pilihan tidak valid!"
+					;;
+		exac
+		;;
+exac
