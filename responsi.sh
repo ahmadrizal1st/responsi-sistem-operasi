5ff01118005e8c4511241bf0c1bc599e3c87123f
eman <emanuelkutiom1@gmail.com>
2025-12-19 21:18:29 +0700

    Responsi Sistem Operasi Bab 1 -6

--- responsi.sh
+++ responsi.sh
@@ -1,1 +1,37 @@
-ISI FILE INI 
+#!/bin/bash
+echo "RESPONSI SISTEM OPERASI"
+echo "nama : eman"
+echo "nim : 241057026"
+echo "==============="
+
+echo "BAB 1 - PERINTAH DASAR LINUX"
+echo "user aktif:"
+whoami
+echo "tanggal sekarang:"
+date
+echo "-----------------"
+
+echo "BAB 2 - SISTEM FILE"
+pwd
+ls 
+echo "-----------------"
+
+echo "BAB 3 - KONFIGURASI LINUX"
+hostname
+uname -a
+echo "-----------------"
+
+echo " BAB 4 - UTILITAS LINUX"
+echo "cek penggunaan disk:"
+df -h
+echo "-----------------"
+
+echo "BAB 5 -PROSES INPUT OUTPUT"
+echo "contoh redirect output"
+echo "hello linux" > output.txt
+cat output.txt
+echo "----------------"
+
+echo "BAB 6 - MANAJEMEN PROSES"
+ps
+
