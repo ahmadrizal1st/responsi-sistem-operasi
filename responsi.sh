1f7498798f10422240b53f00a9da5a996d2260b8
supagera <yusufarjanto@gmail.com>
2025-12-19 11:48:46 +0000

    autentication of order id

--- responsi.sh
+++ responsi.sh
@@ -1,1 +1,15 @@
-ISI FILE INI 
+#!/bin/bash
+
+pelanggan="Andi Pratama"
+orderid="INV-2025-001"
+
+read -p "Nama pelanggan: " nama
+read -p "Nomor Order: " id
+
+if [[ "$nama" == "$pelanggan" && "$id" == "$orderid" ]]; then
+    echo "✔ Pesanan dikonfirmasi. Barang siap dikirim!"
+else
+    echo "❌ Data pesanan tidak ditemukan!"
+fi
+
+
